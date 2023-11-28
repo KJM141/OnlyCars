9bb6d153152ee3e0ebacda21a76942789f1bc69a
Karan <mukherjeekaran81@gmail.com>
2023-11-29 00:15:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Smooth scrolling for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // Update the current year dynamically in the footer
+    const currentYearElement = document.getElementById('currentYear');
+    if (currentYearElement) {
+        const currentYear = new Date().getFullYear();
+        currentYearElement.textContent = currentYear;
+    }
+});
+
